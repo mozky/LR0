c193f51bd42f0e563b4bd779834d5fd27615c800
Javier Suarez <javiersuarezl@outlook.com>
2016-11-13 13:40:37 -0600

    sumAndMultiply
    Added sum and multiply example.

---
+++ examples/sumAndMultiply.js
@@ -0,0 +1,46 @@
+exports.GRAM = [
+  {
+    "tor": "G",
+    "cion": ["S"]
+  },
+  {
+    "tor": "S",
+    "cion": ["E", "=", "E"]
+  },
+  {
+    "tor": "S",
+    "cion": ["f"]
+  },
+  {
+    "tor": "E",
+    "cion": ["T"]
+  },
+  {
+    "tor": "E",
+    "cion": ["E", "+", "T"]
+  },
+  {
+    "tor": "T",
+    "cion": ["f"]
+  },
+  {
+    "tor": "T",
+    "cion": ["T", "*", "f"]
+  }
+];
+
+exports.ACCI = [
+  { "=" : null, "f" : "d3", "+" : null, "*" : null, "$" : null, "S": "I1", "E" : "I2", "T" : "I4" },
+  { "=" : null, "f" : null, "+" : null, "*" : null, "$" : true, "S": null, "E" : null, "T" : null },
+  { "=" : "d5", "f" : null, "+" : "d6", "*" : null, "$" : null, "S": null, "E" : null, "T" : null },
+  { "=" : "r5", "f" : null, "+" : "r5", "*" : "r5", "$" : "r2", "S": null, "E" : null, "T" : null },
+  { "=" : "r3", "f" : null, "+" : "r3", "*" : "d7", "$" : "r3", "S": null, "E" : null, "T" : null },
+  { "=" : null, "f" : "d3", "+" : null, "*" : null, "$" : null, "S": null, "E" : "I8", "T" : "I4" },
+  { "=" : null, "f" : "d3", "+" : null, "*" : null, "$" : null, "S": null, "E" : null, "T" : "I9" },
+  { "=" : null, "f" : "d10", "+" : null, "*" : null, "$" : null, "S": null, "E" : null, "T" : null },
+  { "=" : null, "f" : null, "+" : "d6", "*" : null, "$" : "r1", "S": null, "E" : null, "T" : null },
+  { "=" : "r4", "f" : null, "+" : "r4", "*" : "d7", "$" : "r4", "S": null, "E" : null, "T" : null },
+  { "=" : "r6", "f" : null, "+" : "r6", "*" : "r6", "$" : "r6", "S": null, "E" : null, "T" : null }
+];
+
+exports.INFO = "Gramatica que acepta operaciones de suma y multiplicacion Ej. 'f=f+f', 'f=f*f+f'";
